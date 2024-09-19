33dacdb050dc789c94a1ea64845f1d70d57b55bf
HalfS0ur <riveras.kendall99@gmail.com>
2024-09-18 20:57:13 -0600

    Cases de instrucciones que se pueden ejecutar

--- agente.sv
+++ agente.sv
@@ -1,11 +1,32 @@
 class agent #(parameter bits = 1, parameter drvrs = 4, parameter pckg_sz = 16, parameter broadcast = {8{1'b1}});
     bus_pckg_mbx #(.drvrs(drvrs), .pckg_sz(pckg_sz)) agnt_drvr_mbx[drvrs];
-    instr_pckg_mbx test_agent_mailbox; //Mailbox del test al agente
+    instr_pckg_mbx test_agent_mbx; //Mailbox del test al agente
+    instruccion instruccion;
 
     task run_agent;
         $display("[%g]  El Agente fue inicializado",$time);
         forever begin
-            //mailbox del test al agente
+            if (test_agent_mbx.num() > 0) begin //Si hay un mensaje en el mailbox
+                $display("[%g]  Agente: se recibe instruccion del test",$time);
+                test_agent_mbx.get(instruccion); //Se saca la instruccion del mailbox
+                case(instruccion)
+                    aleatorio: begin
+                        
+                    end
+
+                    broadcast: begin
+                        
+                    end
+
+                    retardos: begin
+                        
+                    end
+
+                    especifico: begin
+                        
+                    end
+                endcase
+            end
         end
     endtask
 endclass
