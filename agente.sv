1ad70345121d4f694dc9b18d3399a07e2493515f
HalfS0ur <riveras.kendall99@gmail.com>
2024-09-18 20:05:44 -0600

    task para correr el agente

--- agente.sv
+++ agente.sv
@@ -1,2 +1,8 @@
 class agent #(parameter bits = 1, parameter drvrs = 4, parameter pckg_sz = 16, parameter broadcast = {8{1'b1}});
+    bus_pckg_mbx #(.drvrs(drvrs), .pckg_sz(pckg_sz)) agnt_drvr_mbx[drvrs];
+    //Necesito un mailbox del test al agente
+
+    task run_agent;
+        $display("[%g]  El Agente fue inicializado",$time);
+    endtask
 endclass
