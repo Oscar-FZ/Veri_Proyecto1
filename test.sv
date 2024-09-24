547296d067d9dc488f744b463241d6bb7bd0d99a
HalfS0ur <riveras.kendall99@gmail.com>
2024-09-24 14:54:47 -0600

    I'm a bit lost :(

--- test.sv
+++ test.sv
@@ -4,4 +4,17 @@ instr_pckg_mbx test_agent_mbx;
 //Definición del ambiente de la prueba
 ambiente #(.bits(bits), .drvrs(drvrs), .pckg_sz(pckg_sz), .broadcast(broadcast)) ambiente_inst;
 
+//Definición de la interfaz para conectar el DUT
+virtual bus_if #(.bits(bits), .drvrs(drvrs), .pckg_sz(pckg_sz)) _if;
+
+//Definición de las condiciones iniciales del test
+function new();
+    //Instanciación de los mailboxes
+    test_agent_mbx = new();
+
+    //Definición y conexión del driver
+    ambiente_inst = new();
+    ambiente_inst._if = _if;
+    ambiente_inst.test_agent_mbx = test_agent_mbx;
+endfunction
 endclass
