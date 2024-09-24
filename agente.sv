24c81013219a67849deb156deee340f45e280f15
HalfS0ur <riveras.kendall99@gmail.com>
2024-09-24 13:27:29 -0600

    aver

--- agente.sv
+++ agente.sv
@@ -38,7 +38,7 @@ class agent #(parameter bits = 1, parameter drvrs = 4, parameter pckg_sz = 16, p
                         transaccion = new;
                         transaccion.max_retardo = max_retardo;
                         transaccion.direccion = broadcast; //testing
-                        transaccion.randomize();
+                        //transaccion.randomize();
                         transaccion.dato = {transaccion.direccion, transaccion.info};
                         transaccion.print("[PRUEBA]");
                         agnt_drvr_mbx[transaccion.dispositivo].put(transaccion);
