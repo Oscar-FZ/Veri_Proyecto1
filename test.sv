ac0067e74c0d6a382e4d7bb9adaf83231f20f7c3
HalfS0ur <riveras.kendall99@gmail.com>
2024-09-24 19:09:26 -0600

    Estructura del test

--- test.sv
+++ test.sv
@@ -7,6 +7,8 @@ ambiente #(.bits(bits), .drvrs(drvrs), .pckg_sz(pckg_sz), .broadcast(broadcast))
 //Definición de la interfaz para conectar el DUT
 virtual bus_if #(.bits(bits), .drvrs(drvrs), .pckg_sz(pckg_sz)) _if;
 
+instruccion trans_agente;
+
 //Definición de las condiciones iniciales del test
 function new();
     //Instanciación de los mailboxes
@@ -24,9 +26,23 @@ function new();
     ambiente_inst.agent_inst.tpo_spec = //TODO
     ambiente_inst.agent_inst.dsp_spec = //TODO
     ambiente_inst.agent_inst.dir_spec = //TODO
+    //Tal vez agregar max retardo
 endfunction
 
 task run;
+    $display("[%g] Test inicializado", $time);
+    fork
+        ambiente_inst.run();
+    join_none
+
+    trans_agente = aleatorio;
+    test_agent_mbx.put(trans_agente);
+    $display("[%g] Test: Enviada la instrucción de transacción aleatoria", $time);
+
+    #10000
+    $display("[%g] Test: Se alcanzó el tiempo límite de la prueba", $time);
+    #20;
+    $finish;
 //TODO
 //Envio de paquetes aleatorios, envio de paquetes broadcast, envio de reset, 
 //paquetes aleatorios desde diferentes disp. al mismo tiempo,
