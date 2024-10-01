2a3033f08f8519a35a7d725d4414ea60b9e2de2f
HalfS0ur <riveras.kendall99@gmail.com>
2024-10-01 00:47:46 -0600

    Format scoreboard

--- scoreboard.sv
+++ scoreboard.sv
@@ -29,9 +29,9 @@ class scoreboard #(parameter bits = 1, parameter drvrs = 4, parameter pckg_sz = 
         test_aleatorio = $fopen(nombre_archivo, "w");
         $fwrite(test_aleatorio, "Test: ", tipo_test, "\n");
         $fwrite(test_aleatorio, "Parametros del Ambiente\n");
-        $fwrite(test_aleatorio, "Bits = %i\n", bits);
-        $fwrite(test_aleatorio, "Drivers = %i\n", drvrs);
-        $fwrite(test_aleatorio, "Tamaño del Paquete = %i\n", pckg_sz);
+        $fwrite(test_aleatorio, "Bits = %0d\n", bits);
+        $fwrite(test_aleatorio, "Drivers = %0d\n", drvrs);
+        $fwrite(test_aleatorio, "Tamaño del Paquete = %0d\n", pckg_sz);
         $fwrite(test_aleatorio, "Identificador de Broadcast = %b\n", broadcast);
         $fclose(test_aleatorio);
         inicio = 0;
