d07db4505fbfe3584ab109fd480a951d1a98286b
HalfS0ur <riveras.kendall99@gmail.com>
2024-10-01 22:53:11 -0600

    Force

--- transactions.sv
+++ transactions.sv
@@ -149,6 +149,6 @@ typedef mailbox #(bus_pckg #(.drvrs(4), .pckg_sz(16))) bus_pckg_mbx;
 typedef mailbox #(sb_pckg #(.drvrs(4), .pckg_sz(16))) sb_pckg_mbx;
 typedef mailbox #(instruccion) instr_pckg_mbx; //Este lo voy a usar para conectar el test con el agente
 typedef mailbox #(int) trans_data; //Ver si le cambio el nombre
-typedef mailbox #(string) test_type; //Referencia a Evangelion
+typedef mailbox #(string) test_type; //Referencia a Evangelion!
 
 event fin_test;
