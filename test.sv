e37daaf8918167fd0cb2f5b6338cd66fcc7be993
HalfS0ur <riveras.kendall99@gmail.com>
2024-10-02 22:31:38 -0600

    try that

--- test.sv
+++ test.sv
@@ -101,7 +101,7 @@ class test #(parameter bits = 1, parameter drvrs = 4, parameter pckg_sz = 16, pa
         test_agent_mbx.put(trans_agente);
         test_sb_mbx.put(tipo_test);
         $display("[%g] Test: Enviada la instrucción de envío de paquetes con maxima alternancia", $time);
-        //sb_test_flag_mbx.get(flag);
+        sb_test_flag_mbx.get(flag);
         //$finish;
         //-------------------------------------------------------------------------------------------
 
