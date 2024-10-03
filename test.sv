fda335329fbf9992ebf66bb7b8087966ef8ca0b5
HalfS0ur <riveras.kendall99@gmail.com>
2024-10-02 22:08:29 -0600

    Added missing ;

--- test.sv
+++ test.sv
@@ -75,7 +75,7 @@ class test #(parameter bits = 1, parameter drvrs = 4, parameter pckg_sz = 16, pa
         //-------------------------------------------------------------------------------------------
         //Prueba de maxima alternancia
         trans_agente = max_alt;
-        tipo_test = "Maxima Alternancia"
+        tipo_test = "Maxima Alternancia";
         test_agent_mbx.put(trans_agente);
         test_sb_mbx.put(tipo_test);
         $display("[%g] Test: Enviada la instrucción de envío de paquetes con maxima alternancia", $time);
